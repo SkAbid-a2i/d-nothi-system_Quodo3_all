5adfb133814a9ac7a0c035ff22f8e4ea011964f4
SkAbid-a2i <sk.abid@a2i.gov.bd>
2025-11-11 16:49:31 +0600

    Trigger new build to fix ESLint error

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,1 +1,13 @@
+// Add comment to trigger new build
+// ESLint fix for useEffect dependency
+import React, { useState, useEffect } from 'react';
+
+const Chat = ({ user }) => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    // Reconnect to the chat service when user changes
   }, [user]); // Only reconnect when user changes
+};
+
+export default Chat;
